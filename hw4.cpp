86d15d11abff2d5ac8a7c9d5ab69afac8f6d9bc5
Jorge Celaya <jcelaya775@gmail.com>
2022-04-24 15:23:15 -0500

    update

--- hw4.cpp
+++ hw4.cpp
@@ -7,11 +7,11 @@
 
 using namespace std;
 
-class coordinate {
+class Coordinate {
     public:
         int x;
         int y;
-        coordinate(int x, int y) {
+        Coordinate(int x, int y) {
             this->x = x;
             this->y = y;
         }
@@ -25,7 +25,7 @@ class coordinate {
         }
 };
 
-int getMaxPolyArea(vector<coordinate>);
+int getMaxPolyArea(vector<Coordinate>);
 
 int main() {
     cout << "Enter input file: ";
@@ -42,32 +42,57 @@ int main() {
     int n;
     inputFile >> n;
 
-    vector<coordinate> coordinates;
+    vector<Coordinate> coordinates;
 
     int i = 0;
     while (i < n) {
         int x, y;
         inputFile >> x;
         inputFile >> y;
-        coordinates.push_back(coordinate(x, y));
+        coordinates.push_back(Coordinate(x, y));
         i++;
     }
 
-    cout << "\nmax area = " << getMaxPolyArea(coordinates) << endl;
+    int maxPolyArea = getMaxPolyArea(coordinates);
+    cout << "\nmax area = " << maxPolyArea << endl;
     
     return 0;
 }
 
-int getMaxPolyArea(vector<coordinate> coordinates) {
-    stack<coordinate> cstack;
-
+int getMaxPolyArea(vector<Coordinate> coordinates) {
     int maxArea = 0;
+
+    stack<Coordinate> coordStack;
+
     for (int i = 0; i < coordinates.size(); i++) {
-        int currPos = coordinates[i].getX();
-        int currHeight = coordinates[i].getY();
+        cout << "i: " << i << endl;
+        int pos = coordinates[i].getX();
+        int height = coordinates[i].getY();
 
-        int lastPos = INT_MAX;
+        cout << "x-pos = " <<  pos << ", height = " << height << endl;
 
+        int startPos = INT_MAX;
+        // height decreases -> compute area(s)
+        while (!coordStack.empty() && height < coordStack.top().getY()) {
+            cout << "pop stack: (" << coordStack.top().getX() << ", " << coordStack.top().getY() << ")" << endl;
+            startPos = coordStack.top().getX();
+            int currArea = height * (pos - coordStack.top().getX());
+
+            coordStack.pop();
+
+            maxArea = max(currArea, maxArea);
+
+            cout << "current area = " << currArea << endl;            
+            cout << "nex max area = " << maxArea << endl;
+        }
+
+        // height increases -> push coordinate to stack
+        if (coordStack.empty() || height > coordStack.top().getY()) {
+            coordStack.push(Coordinate(min(startPos, pos), height));
+            cout << "push stack: (" << coordStack.top().getX() << ", " << coordStack.top().getY() << ")" << endl;
+        }
+
+        cout << endl;
     }
 
     return maxArea;
